2a841164b85a5b04b3b1dae3f89f0f41b96aff14
FalsePattern <me@falsepattern.com>
2024-07-17 18:32:28 +0200

    Getting to the end is not what's important

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("fpgradle-minecraft") version ("0.6.1")
+}
+
+group = "cofh"
+
+minecraft_fp {
+    mod {
+        modid = "CoFHCore"
+        name = "CoFH Core"
+        rootPkg = "$group"
+    }
+
+    api {
+        packages = listOf("api", "lib")
+    }
+
+    core {
+        coreModClass = "asm.LoadingPlugin"
+        accessTransformerFile = "CoFH_at.cfg"
+    }
+
+    tokens {
+        tokenClass = "Tags"
+    }
+}
+
+repositories {
+    mega {
+        content {
+            includeGroup("codechicken")
+        }
+    }
+}
+
+dependencies {
+    implementation("codechicken:codechickencore-mc1.7.10:1.4.0-mega:dev")
+    implementation("codechicken:notenoughitems-mc1.7.10:2.3.1-mega:dev")
+}
